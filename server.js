54e54c3a5c69d4af503cb3ffaca741655b992638
Chad33 <chad.javarush@gmail.com>
2018-06-25 21:01:15 -0600

    Made changes to server.js, troubleshooting for heroku

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 //Dependencies
-var express = require("express");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
-var axios = require("axios");
-var cheerio = require("cheerio");
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 // Require all models
-var db = require("./models");
+const Article = require("./models/article");
+const Note = require("./models/note");
 
 
-var PORT = process.env.PORT || 8080 || process.env.MONGOLAB_URI;
+const PORT = process.env.PORT || 3000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 // Initialize Express
-var app = express();
+const app = express();
 
 // morgan creates a log of requests
 app.use(logger("dev"));
@@ -24,7 +29,7 @@ app.use(express.static("public"));
 
 // This connects to the Mongo DB
 // mongoose.connect("mongodb://localhost/scraper");
-mongoose.connect("mongodb://yellingfire:303scraper@ds153700.mlab.com:53700/heroku_lvbq4gz6");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://yellingfire:303scraper@ds153700.mlab.com:53700/heroku_lvbq4gz6");
 var db = mongoose.connection
 // Routes
 
@@ -50,7 +55,7 @@ app.get("/scrape", function(req, res) {
         .attr("href");
 
       // this creates a new Article from the scrape
-      db.Article.create(result)
+      Article.create(result)
         .then(function(dbArticle) {
     
           console.log(dbArticle);
@@ -68,7 +73,7 @@ app.get("/scrape", function(req, res) {
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
   // Grab every document in the Articles collection
-  db.Article.find({})
+  Article.find({})
     
     .then(function(dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
@@ -84,7 +89,7 @@ app.get("/articles", function(req, res) {
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article.findOne({ _id: req.params.id })
+  Article.findOne({ _id: req.params.id })
     // ..and populate all of the notes associated with it
     .populate("note")
     .then(function(dbArticle) {
@@ -100,12 +105,12 @@ app.get("/articles/:id", function(req, res) {
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
   // Create a new note and pass the req.body to the entry
-  db.Note.create(req.body)
+  Note.create(req.body)
     .then(function(dbNote) {
       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      return Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
     .then(function(dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
@@ -123,7 +128,7 @@ app.delete("/delete/:id", function(req, res) {
   // console.log("+++++++ Delete route hit in server.js req.params: " + req.params.id + "+++++++")
  
   // Remove a note using the objectID
-    db.Note.deleteOne({ _id: req.params.id }, function(error, removed) {     
+    Note.deleteOne({ _id: req.params.id }, function(error, removed) {     
         
       if (error) {
         // console.log(error);
@@ -132,7 +137,7 @@ app.delete("/delete/:id", function(req, res) {
       else {
         // Otherwise, send the mongojs response to the browser
         // This will fire off the success function of the ajax request
-        db.Article.findOne({}, (error, found => {
+        Article.findOne({}, (error, found => {
           // console.log("found" + found);
         })); 
         // console.log("This is the removed 'else': " + removed);
